774d0324d8d163fe87b7851161a941e1353b5a6f
rushfe <97265280+rushfe@users.noreply.github.com>
2022-04-24 19:27:38 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -14,7 +14,7 @@ const Footer = ({ fullWidth }) => {
       <div className="my-4 text-sm leading-6">
         <div className="flex align-baseline justify-between flex-wrap">
           <p>
-            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
+            © 放在冰箱的速冻水饺 {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
           </p>
           <Vercel />
         </div>
